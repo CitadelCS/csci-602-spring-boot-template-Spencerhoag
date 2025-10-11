04bfeae436d305627c789812740a6ba457306ee6
Spencerhoag <97331670+Spencerhoag@users.noreply.github.com>
2025-10-10 22:26:59 -0400

    cucumber tests

---
+++ src/test/java/edu/citadel/main/cucumber/SpringIntegrationTest.java
@@ -0,0 +1,10 @@
+package edu.citadel.main.cucumber;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringIntegrationTest {
+
+}
